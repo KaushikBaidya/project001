e004e78cc3ab15ef0287a9a7c91e45d75695cdc4
KaushikBaidya <47146123+KaushikBaidya@users.noreply.github.com>
2021-08-15 06:51:00 +0600

    01

--- index.js
+++ index.js
@@ -17,20 +17,29 @@ app.use(express.urlencoded({
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
-// const adminRoutes = require('./routes/admin.js')
-// const shopRoutes = require('./routes/shop.js')
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
   
 // app.use(bodyParser.urlencoded({ extended: false }))
 // app.use(express.json())
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-// app.use('/admin', adminRoutes)
-// app.use(shopRoutes)
+app.use((req, res, next) => {
+  // User.findById(1)
+  //   .then(user => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch(err => console.log(err));
+  next();
+});
+
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
 
 app.use(errorController.get404)
 
 mongoConnect(client => {
-  console.log(client)
   app.listen(3000)
 })
--- util/database.js
+++ util/database.js
@@ -1,13 +1,25 @@
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
 
+let _db
+
 const mongoConnect = callback => {
-    MongoClient.connect('mongodb+srv://dbUser:F*6ucZhkx3YwU7b@cluster0.obba8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(client => {
+    MongoClient.connect('mongodb+srv://dbUser:F*6ucZhkx3YwU7b@cluster0.obba8.mongodb.net/shop?retryWrites=true&w=majority').then(client => {
         console.log('connected')
-        callback(client)
+        _db = client.db
+        callback()
     }).catch(err => {
         console.log(err)
+        throw err
     })
 }
 
-module.exports = mongoConnect
+const getDb = () => {
+    if (_db) {
+        return _db
+    }
+    throw ('No database found!')
+}
+
+exports.mongoConnect = mongoConnect
+exports.getDb = getDb
