0584dfc0177089c8a27da9459df36858cf6fb860
KaushikBaidya <47146123+KaushikBaidya@users.noreply.github.com>
2021-08-18 12:26:15 +0600

    .

--- models/user.js
+++ models/user.js
@@ -67,31 +67,47 @@ class User {
 
   deleteItemFromCart(productId) {
     const updatedCartItems = this.cart.items.filter(item => {
-      return item.productId.toString() !== productId.toString()
-    })
+      return item.productId.toString() !== productId.toString();
+    });
     const db = getDb();
     return db
       .collection('users')
       .updateOne(
         { _id: new ObjectId(this._id) },
-        { $set: { cart: {items: updatedCartItems} } }
+        { $set: { cart: { items: updatedCartItems } } }
       );
   }
 
   addOrder() {
-    const db = getDb()
-    return db
-      .collection('orders')
-      .insertOne(this.cart)
+    const db = getDb();
+    return this.getCart()
+      .then(products => {
+        const order = {
+          items: products,
+          user: {
+            _id: new ObjectId(this._id),
+            name: this.name
+          }
+        };
+        return db.collection('orders').insertOne(order);
+      })
       .then(result => {
-        this.cart = { items: [] }
+        this.cart = { items: [] };
         return db
           .collection('users')
           .updateOne(
-            { _id: new ObjectId(this.id) },
-            { $set : { cart: { items: [] } } }
-          )
-      })
+            { _id: new ObjectId(this._id) },
+            { $set: { cart: { items: [] } } }
+          );
+      });
+  }
+
+  getOrders() {
+    const db = getDb();
+    return db
+      .collection('orders')
+      .find({ 'user._id': new ObjectId(this._id) })
+      .toArray();
   }
 
   static findById(userId) {
